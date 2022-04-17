d0b4782d3fbacdd480f1c14803961d1fbd53b44b
ramarav <ramarav@gmail.com>
2022-04-17 22:49:05 +0530

    lists

---
+++ 2.Variables and types/1Numbers_example.py
@@ -0,0 +1,13 @@
+#Integer Example
+myint=5
+print("My Integer is : ",myint)
+
+#Floating Pont number
+myfloat = 4.7
+print("My floating point number is : ",myfloat)
+
+#Simple operations on numbers
+one = 1
+two = 2
+three = one+two
+print("One + Two is : ",three)
---
+++ 2.Variables and types/3simultaneous_assignment.py
@@ -0,0 +1,11 @@
+#Simultaneous assignment is possible
+a,b,c,d = 1,2,3,4
+print(a)
+print(b)
+print(c)
+print(d)
+
+#Mixing operators between numbers and strings is not possible
+mystring = "hello"
+#print(a+b+mystring)
+#TypeError: unsupported operand type(s) for +: 'int' and 'str'
