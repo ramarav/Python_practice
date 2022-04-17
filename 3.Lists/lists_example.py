4d78e604a0737efbbdb5024f9740eee216a99f28
ramarav <ramarav@gmail.com>
2022-04-17 19:01:59 +0530

    Lists

---
+++ 3.Lists/lists_example.py
@@ -0,0 +1,16 @@
+#Lists are similar to arrays
+#Lists can be iterated
+myList = []
+myList.append(1)
+myList.append(2)
+myList.append(3)
+
+print(myList)
+
+#test code
+for i in myList:
+    print('List contains',i)
+
+#test code 2
+myList = [1,2,3,4,5]
+print(myList[0:2])
