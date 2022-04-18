0c7f4031a511abe6203167f7b908a2fbb24e0168
ramarav <ramarav@gmail.com>
2022-04-18 10:58:13 +0530

    conditions

---
+++ 7.Conditions/1conditions.py
@@ -0,0 +1,14 @@
+# True or False
+x=2
+print('x = %d'%x)
+print(x!=2)
+print(x!=4)
+print(x<6)
+
+# Boolean conditions
+name = 'Rama'
+age = 34
+if name == 'Rama':
+    print("That's an awesome name")
+if age<40:
+    print('You are awesome')
