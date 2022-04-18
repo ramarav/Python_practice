2a512b6ad92851d5bf3ecab94c8cd0ecdd37ae8c
ramarav <ramarav@gmail.com>
2022-04-18 18:15:05 +0530

    dictionary

---
+++ 11.Dictionaries/1dict_basics.py
@@ -0,0 +1,9 @@
+# Dictionary is a data type similar to arrays, but instead of indexes, it uses keys and values.
+# Dictionary values can be accessed using keys,
+# which are any types of objects (a string, a number, a list, etc.) rather than indexes.
+
+phonebook = {}
+phonebook["Rama"] = 9848
+phonebook["Rao"] = 9948
+phonebook["Mekala"] = 9949
+print(phonebook)
---
+++ 11.Dictionaries/2alternate_dict.py
@@ -0,0 +1,6 @@
+phonebook = {
+    "Rama": 9848,
+    "Rao": 9948,
+    "Mekala" : 9949
+}
+print(phonebook)
