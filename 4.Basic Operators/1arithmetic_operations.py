e1cc35a244a175c305c46d48c231101df2ed5ec2
ramarav <ramarav@gmail.com>
2022-04-18 09:24:18 +0530

    arithmetic operators

---
+++ 4.Basic Operators/1arithmetic_operations.py
@@ -0,0 +1,16 @@
+"""
+addition, subtraction, multiplication, and division operators can be used with numbers
+"""
+a =1
+b = 2
+c=3
+sum = a+b
+diff = a-b
+mul = a*b
+div = a/b
+print(sum)
+print(diff)
+print(mul)
+print(div)
+
+
