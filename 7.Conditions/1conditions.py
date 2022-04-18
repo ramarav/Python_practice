d472eccb41cb5ba90bbc0a69a10851df4d44e363
ramarav <ramarav@gmail.com>
2022-04-18 11:20:27 +0530

    Conditions

--- 7.Conditions/1conditions.py
+++ 7.Conditions/1conditions.py
@@ -8,7 +8,10 @@ print(x<6)
 # Boolean conditions
 name = 'Rama'
 age = 34
-if name == 'Rama':
-    print("That's an awesome name")
-if age<40:
-    print('You are awesome')
+if name == 'Rama' and age ==34:
+    print("You are awesome")
+if name == 'Rama' or name == 'Rao':
+    print("That's a great name")
+# in operator
+if name in ["Rama","Rao", "Mekala"]:
+    print("That is one good name")
---
+++ 7.Conditions/2codeblocks.py
@@ -0,0 +1,15 @@
+"""
+Python uses indentation to define a block of code.
+It won't use parenthesis.
+"""
+x=5
+if x<8:
+    print('x is less than 8')
+else:
+    print('x is greater than 8')
+# is operator
+x=3
+y=4
+print(x is y)
+# not operator
+print(not y)
