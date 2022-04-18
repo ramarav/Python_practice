de171d6a03c43a6e4f991a518ee72e37f3574875
ramarav <ramarav@gmail.com>
2022-04-18 11:41:33 +0530

    Create 1for_loop.py

---
+++ 8.Loops/1for_loop.py
@@ -0,0 +1,12 @@
+# For loops iterate over a given sequence.
+x = [1,2,3,4,5]
+for i in x:
+    print (i)
+
+#For loops can iterate over a sequence of numbers using the "range" functions.
+for i in range(10):
+    print (i)
+for i in range(5,8):
+    print (i)
+for i in range(5,10,2):
+    print (i)
