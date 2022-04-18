144e0f1385b64b4f445d8ec4e63dba1cf1758d27
ramarav <ramarav@gmail.com>
2022-04-18 18:31:11 +0530

    Create 4dict_remove.py

---
+++ 11.Dictionaries/4dict_remove.py
@@ -0,0 +1,8 @@
+# There del method and pop method for this purpose
+
+phonebook = {"Rama": 9848, "Rao": 9948, "Mekala": 9949}
+phonebook.pop("Rao")
+print(phonebook)
+
+del phonebook["Mekala"]
+print(phonebook)
