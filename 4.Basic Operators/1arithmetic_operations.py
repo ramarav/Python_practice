1ba5b4d874a99f5a540194e9884c692cb54cb5e4
ramarav <ramarav@gmail.com>
2022-04-18 09:29:47 +0530

    String Operations

--- 4.Basic Operators/1arithmetic_operations.py
+++ 4.Basic Operators/1arithmetic_operations.py
@@ -1,16 +1,23 @@
 """
 addition, subtraction, multiplication, and division operators can be used with numbers
 """
-a =1
+a = 1
 b = 2
-c=3
-sum = a+b
-diff = a-b
-mul = a*b
-div = a/b
-print(sum)
+c = 3
+sum1 = a + b
+diff = a - b
+mul = a * b
+div = a / b
+
+print(sum1)
 print(diff)
 print(mul)
 print(div)
 
+# Power
+power = pow(a, b)
+print(power)
 
+#Modulo %
+remainder = a%b
+print(remainder)
---
+++ 4.Basic Operators/2string_operators.py
@@ -0,0 +1,8 @@
+#string concatenation
+string1 = " Hello"
+string2 = "World"
+myString = string1+" "+string2
+print(myString)
+
+#Multiplication operation on strings
+print(myString*5)
