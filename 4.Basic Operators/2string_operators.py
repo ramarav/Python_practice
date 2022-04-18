81d2d0d84962732d691caa16a2a282b1a8918c84
ramarav <ramarav@gmail.com>
2022-04-18 09:30:23 +0530

    Update 2string_operators.py

--- 4.Basic Operators/2string_operators.py
+++ 4.Basic Operators/2string_operators.py
@@ -1,8 +1,8 @@
-#string concatenation
+# string concatenation
 string1 = " Hello"
 string2 = "World"
-myString = string1+" "+string2
+myString = string1 + " " + string2
 print(myString)
 
-#Multiplication operation on strings
-print(myString*5)
+# Multiplication operation on strings
+print(myString * 5)
